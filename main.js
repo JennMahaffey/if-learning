bc7e7811472344391ddfb94218d9e2d54ed71a15
jennifer mahaffey <jmahaffey1.prsvr@gmail.com>
2023-09-05 10:19:33 -0400

    2nd commit

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ function isItLong(str){
   if(str.length > 20){
     return "That's a long string!";
   }
+  function isItMedium(str){
+    if(str.length >= 10 && str.length <= 20){
+      return "That's a regular sized string!";
+    }
+  }
 
 
 
