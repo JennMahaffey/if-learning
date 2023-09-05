dd5799cfbbf359d35782f707ea34f7f0f3f0d560
jennifer mahaffey <jmahaffey1.prsvr@gmail.com>
2023-09-05 10:18:48 -0400

    init commit

---
+++ main.js
@@ -0,0 +1,46 @@
+/******************************
+ * YOUR CODE GOES RIGHT HERE! *
+ ******************************/
+function isItLong(str){
+  if(str.length > 20){
+    return "That's a long string!";
+  }
+
+
+
+
+
+/**********************************
+ *      OUR CODE GOES BELOW.      *
+ * THIS IS FOR INTERNAL USE ONLY. *
+ * DO NOT ADD OR CHANGE ANYTHING! *
+ **********************************/
+
+if (typeof isItLong === 'undefined') {
+  isItLong = undefined;
+}
+
+if (typeof isItMedium === 'undefined') {
+  isItMedium = undefined;
+}
+
+if (typeof isItShort === 'undefined') {
+  isItShort = undefined;
+}
+
+if (typeof howLongIsMyString === 'undefined') {
+  howLongIsMyString = undefined;
+}
+
+if (typeof instructorHeight === 'undefined') {
+  instructorHeight = undefined;
+}
+
+
+module.exports = {
+  isItLong,
+  isItMedium,
+  isItShort,
+  howLongIsMyString,
+  instructorHeight,
+}
