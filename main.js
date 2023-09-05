db2cf65cacfe62141840683f758314a0ec265315
jennifer mahaffey <jmahaffey1.prsvr@gmail.com>
2023-09-05 10:20:58 -0400

    4th commit

--- main.js
+++ main.js
@@ -17,6 +17,14 @@ function isItLong(str){
       return "That's not a small string!";
     }
   }
+  function howLongIsMyString(str){
+    if(str.length > 20){
+      return "That's a long string!";
+    }else if(str.length >= 10 && str.length <= 20){
+      return "That's a regular sized string!";
+    }else if(str.length < 10){
+      return "That's a small string!";
+    } 
 }
 
 
